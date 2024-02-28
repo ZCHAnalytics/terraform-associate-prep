00afc5b8c0c0c25c1febf6a6360ea5fd22cd84a0
ZCHAnalytics <zulfia.satimbai@gmail.com>
2024-02-28 15:58:16 +0000

    Add AWS credential variables

--- providers.tf
+++ providers.tf
@@ -9,7 +9,8 @@ terraform {
 provider "aws" {
   region  = var.region
   profile = "default"
-  access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
+  access_key = terraform.workspace.variables["TF_VAR_AWS_ACCESS_KEY_ID"]
+  secret_key = terraform.workspace.variables["TF_VAR_AWS_SECRET_ACCESS_KEY"]
+
 }
 
