a7dcd0f8ce188476c13a307963eef8019f6a9d15
ZCHAnalytics <zulfia.satimbai@gmail.com>
2024-02-28 21:08:44 +0000

    Add autoscaler

--- variables.tf
+++ variables.tf
@@ -7,3 +7,26 @@ variable "my_ip_address" {
   type        = string
   description = "The IP address to be used"
 }
+
+variable "environment" {
+  description = "Deployment environment"
+
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
+  default     = 1
+}
+
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
+  default     = 2
+}
